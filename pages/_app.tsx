cd5d00a737efd0638bf23f7c18290ec8ac5fbb97
Felipe Morales <carrionfelipe15@gmail.com>
2022-12-23 19:22:19 -0500

    Login Service

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const domain = process.env.NEXT_PUBLIC_AUTH0_DOMAIN;
+  const clientId = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID;
+
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={process.browser && window.location.origin}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 }
