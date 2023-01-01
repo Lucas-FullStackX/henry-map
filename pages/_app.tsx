10065ad289254919b157207790597f3551011efe
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2023-01-01 16:09:55 -0500

    config mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Auth0Provider } from '@auth0/auth0-react';
-
-export default function App({ Component, pageProps }: AppProps) {
+import { ApolloProvider } from '@apollo/client';
+import { CacheProvider, ThemeProvider, EmotionCache } from '@emotion/react';
+import createEmotionCache from '../styles/motion';
+import lightTheme from '../styles/theme';
+import client from '../src/shared/apollo';
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+const clientSideEmotionCache = createEmotionCache();
+export default function App({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache
+}: MyAppProps) {
   const domain = process.env.NEXT_PUBLIC_AUTH0_DOMAIN;
   const clientId = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID;
 
@@ -12,7 +24,13 @@ export default function App({ Component, pageProps }: AppProps) {
       clientId={clientId}
       redirectUri={process.browser && window.location.origin}
     >
-      <Component {...pageProps} />
+      <ApolloProvider client={client}>
+        <CacheProvider value={emotionCache}>
+          <ThemeProvider theme={lightTheme}>
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </CacheProvider>
+      </ApolloProvider>
     </Auth0Provider>
   );
 }
---
+++ pages/maps-list.tsx
@@ -0,0 +1,23 @@
+import styles from '../styles/Home.module.css';
+// import FocusGraph from '../src/RoadMap/Graph';
+import { useFetchMapListQuery } from '../src/shared/types/generated';
+import { Typography, Container, CircularProgress, Box } from '@mui/material';
+
+export default function MapsList() {
+  const { data, loading } = useFetchMapListQuery();
+  return (
+    <div className={styles.container}>
+      {data && !loading ? (
+        <Container>
+          {data.MapsList.items.map(map => (
+            <Box key={map.id}>
+              <Typography>{map.name}</Typography>
+            </Box>
+          ))}
+        </Container>
+      ) : (
+        <CircularProgress />
+      )}
+    </div>
+  );
+}
