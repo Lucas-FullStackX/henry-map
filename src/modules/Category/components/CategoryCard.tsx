9e51d467dbae13e68dda1c3c6cf8ca61803b1a32
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2023-01-13 18:09:57 -0500

    add next link with mui

--- src/modules/Category/components/CategoryCard.tsx
+++ src/modules/Category/components/CategoryCard.tsx
@@ -1,35 +1,38 @@
-import { Card, Typography, Grid } from '@mui/material';
+import { Card, Typography, Grid, CardActionArea } from '@mui/material';
 import { GetCategoryDetailQuery } from '../../../shared/types/generated';
 import { Box } from '@mui/system';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function CategoryCard({
   roadMap
 }: {
   roadMap: GetCategoryDetailQuery['getCategory']['roadMapsList'][0];
 }) {
+  const router = useRouter();
   return (
     <Grid item xs={12} md={4}>
-      <Card
-        sx={{
-          padding: '15px',
-          height: '20vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-between'
-        }}
-      >
-        <Box
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          sx={{ height: '100%' }}
+      <Card onClick={() => router.push(`/road-map/${roadMap.id}`)}>
+        <CardActionArea
+          sx={{
+            padding: '15px',
+            height: '20vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between'
+          }}
         >
-          <Link href={`/road-map/${roadMap.id}`}>{roadMap.name}</Link>
-        </Box>
-        <Typography variant="subtitle2" textAlign="right">
-          {roadMap.user.name}
-        </Typography>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            sx={{ height: '100%' }}
+          >
+            <Typography variant="h5">{roadMap.name}</Typography>
+          </Box>
+          <Typography variant="subtitle2" textAlign="right">
+            {roadMap.user.name}
+          </Typography>
+        </CardActionArea>
       </Card>
     </Grid>
   );
--- styles/theme.ts
+++ styles/theme.ts
@@ -1,8 +1,19 @@
+import { Roboto } from '@next/font/google';
 import { createTheme } from '@mui/material/styles';
+
+export const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['Helvetica', 'Arial', 'sans-serif']
+});
 
 const lightTheme = createTheme({
   palette: {
     mode: 'light'
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily
   }
 });
 
