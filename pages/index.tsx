8188a2eaea7acd90db13cffbfac066653ac1ba10
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2023-01-14 19:44:27 -0500

    fix landing page styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import LadingPage from '../components/LandingPage/LandingPage';
+import LadingPage from '../src/modules/Landing/LandingPage';
 import NavBar from '../components/NavBar/NavBar';
 
 export default function Home() {
--- src/modules/Landing/LandingPage.tsx
+++ src/modules/Landing/LandingPage.tsx
@@ -6,36 +6,59 @@ import AlignHorizontalLeftIcon from '@mui/icons-material/AlignHorizontalLeft';
 import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 import ShareIcon from '@mui/icons-material/Share';
 import MapIcon from '@mui/icons-material/Map';
-import { Avatar, Grid, Link, Typography } from '@mui/material';
+import { Avatar, Container, Grid, Link, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import { useAuth0 } from '@auth0/auth0-react';
-import logo from '../../img/HENRY_logo_1.png';
+import Image from 'next/image';
 
 export default function LadingPage() {
   const { isAuthenticated, loginWithRedirect } = useAuth0();
   return (
-    <Box className={styles.containerPro}>
-      <div className={styles.circle}>
-        <img src={logo.src} className={styles.img} />
-      </div>
-      <div className={styles.fontLogo}></div>
-      <div className={styles.container}>
-        <h1 className={styles.title}>RoadMap App</h1>
-        <p className={styles.text}>
-          This app is designed so that you have a path to follow when learning
-          <br />
-          to program.
-        </p>
-        {isAuthenticated ? (
-          <Link href="/home">
-            <button className={styles.button}>Get Started</button>
-          </Link>
-        ) : (
-          <button className={styles.button} onClick={() => loginWithRedirect()}>
-            Get Started
-          </button>
-        )}
-      </div>
+    <Container>
+      <Grid
+        container
+        spacing={2}
+        alignItems="center"
+        sx={{
+          height: '80vh'
+        }}
+      >
+        <Grid item md={6} xs={12}>
+          <Typography variant="h1" fontWeight={800} gutterBottom>
+            RoadMap App
+          </Typography>
+          <Typography variant="body2" color="text.secondary" gutterBottom>
+            This app is designed so that you have a path to follow when learning
+            to program.
+          </Typography>
+          {isAuthenticated ? (
+            <Link href="/home">
+              <button className={styles.button}>Get Started</button>
+            </Link>
+          ) : (
+            <button
+              className={styles.button}
+              onClick={() => loginWithRedirect()}
+            >
+              Get Started
+            </button>
+          )}
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <div className={styles.circle}>
+            <Image
+              src="/img/henry-logo-cpy.png"
+              alt="henry"
+              width={300}
+              height={300}
+              style={{
+                objectFit: 'contain',
+                marginLeft: '20px'
+              }}
+            />
+          </div>
+        </Grid>
+      </Grid>
       <h2 className={styles.paths}>Discover the learning paths</h2>
       {/* <div className={styles.circle2}></div> */}
       <Box sx={{ marginLeft: '60px' }}>
@@ -162,6 +185,6 @@ export default function LadingPage() {
           </Grid>
         </div>
       </div>
-    </Box>
+    </Container>
   );
 }
